81f76e88c195aa03a2cbfc006c1749231e562b00
aakash <aakash@LAPTOP-LG0G9M8A>
2024-02-14 18:46:18 +0530

    show balance feature added

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,22 @@
-actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
-    };
-};
+import Principal "mo:base/Principal";
+import HashMap "mo:base/HashMap";
+
+
+actor Token{
+
+    var owner : Principal = Principal.fromText("52ten-jmxb2-fhi4t-w4znc-tckux-d5c2c-gacv5-teoum-on64e-w2gxc-gae");
+    var totalSupply:Nat = 1000000000;
+
+    var balances = HashMap.HashMap<Principal,Nat>(1,Principal.equal,Principal.hash);
+    balances.put(owner,totalSupply);
+
+    public query func balanceOf(who:Principal) : async Nat{
+        var balance = switch(balances.get(who)) {
+            case(null) {0};
+            case(?result) {result};
+        };
+        return balance;
+    }
+
+
+}
---
+++ src/token_assets/src/components/Balance.jsx
@@ -0,0 +1,42 @@
+import React,{useState} from "react";
+import {Principal} from "@dfnity/Principal";
+import {token} from "../../../declarations/token";
+
+function Balance() {
+
+  const [principal, setPrincipal] = useState("");
+  const [balRes, setbalRes] = useState("");
+  
+  async function handleClick() {
+    // console.log("Balance Button Clicked");
+    const bal = await token.balanceOf(Principal.fromText(principal));
+    setbalRes(bal.toLocaleString());
+  }
+
+
+  return (
+    <div className="window white">
+      <label>Check account token balance:</label>
+      <p>
+        <input
+          id="balance-principal-id"
+          type="text"
+          placeholder="Enter a Principal ID"
+          value={principal}
+          onChange={(e)=>{setPrincipal(e.target.value)}}
+        />
+      </p>
+      <p className="trade-buttons">
+        <button
+          id="btn-request-balance"
+          onClick={handleClick}
+        >
+          Check Balance
+        </button>
+      </p>
+      <p>This account has a balance of {balRes}.</p>
+    </div>
+  );
+}
+
+export default Balance;
